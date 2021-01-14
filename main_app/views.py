2798e844b977321e1c6263c359a54f530f614f5b
Rome Bell <romebell5@gmail.com>
2021-01-14 09:46:37 -0800

    Add model and cats to database

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,5 @@
 from django.shortcuts import render
-
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-        
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+from .models import Cat
 
 # Create your views here.
 def index(request):
@@ -22,4 +9,5 @@ def about(request):
   return render(request, 'about.html')
 
 def cats_index(request):
+  cats = Cat.objects.all()
   return render(request, 'cats/index.html', {'cats': cats})
