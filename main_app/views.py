152dd749f5361dde23a5003fb955c2ddfccb8fca
Tyler Dance <tylerdance@Tylers-iMac.fios-router.home>
2021-01-14 16:41:50 -0500

    codealong

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Cat
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -8,6 +11,8 @@ def index(request):
 def about(request):
   return render(request, 'about.html')
 
+
+########### CATS ############
 def cats_index(request):
   cats = Cat.objects.all()
   return render(request, 'cats/index.html', { 'cats': cats })
@@ -15,3 +20,34 @@ def cats_index(request):
 def cats_show(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
   return render(request, 'cats/show.html', { 'cat': cat })
+
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+  success_url = '/cats'
+
+  def form_valid(self, form):
+    self.object = form.save(commit=False)
+    self.object.user = self.request.user
+    self.object.save()
+    return HttpResponseRedirect('/cats')
+
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['name', 'breed', 'description', 'age']
+  
+  def form_valid(self, form):
+    self.object = form.save(commit=False)
+    self.object.save()
+    return HttpResponseRedirect('/cats')
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats'
+
+
+####### USER #######
+def profile(request, username):
+  user = User.objects.get(username=username)
+  cats = Cat.object.filter(user=user)
+  return render(request, 'profile.html', {'username': username, 'cats': cats})
